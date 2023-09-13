954981ae6fefae4a3369bf967278f9d64ab79567
Joel Gonzalez <joelgzzjr@hotmail.com>
2023-09-12 18:16:13 -0600

    Adding Swagger decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Body, Post, ValidationPipe, Get, UseGuards, Put } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiResponse, ApiOperation, ApiBearerAuth } from '@nestjs/swagger';
 import { SendEmailResponse } from 'aws-sdk/clients/ses';
 
 import { AuthService } from './auth.service';
@@ -8,6 +9,8 @@ import { AuthResetPasswordDto } from './dto/auth-reset-password.dto';
 import { AuthSignInDto } from './dto/auth-signin.dto';
 import { AuthSignUpDto } from './dto/auth-signup.dto';
 import { GetUser } from './get-user.decorator';
+import { TokenResponse } from './response/token.response';
+import { UserResponse } from './response/user.response';
 import { User } from './user.entity';
 
 @Controller('auth')
@@ -15,14 +18,19 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  async signUp(@Body(ValidationPipe) authSignUpDto: AuthSignUpDto): Promise<{ accessToken: string; date: Date }> {
+  @ApiOperation({ summary: 'Sign up a User', tags: ['auth'] })
+  @ApiResponse({ status: 201, description: 'Created', type: TokenResponse })
+  async signUp(@Body(ValidationPipe) authSignUpDto: AuthSignUpDto): Promise<TokenResponse> {
     const { email, password } = authSignUpDto;
     await this.authService.signUp(authSignUpDto);
     return this.authService.signIn({ email, password });
   }
 
   @Post('/signin')
-  async signIn(@Body(ValidationPipe) authSignInDto: AuthSignInDto): Promise<{ accessToken: string; date: Date }> {
+  @ApiOperation({ summary: 'Sign in a User', tags: ['auth'] })
+  @ApiResponse({ status: 201, description: 'Created', type: TokenResponse })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  async signIn(@Body(ValidationPipe) authSignInDto: AuthSignInDto): Promise<TokenResponse> {
     return await this.authService.signIn(authSignInDto);
   }
 
@@ -38,7 +46,11 @@ export class AuthController {
 
   @UseGuards(AuthGuard())
   @Get('/me')
-  me(@GetUser() user: User): User {
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Get User info', tags: ['auth'] })
+  @ApiResponse({ status: 201, description: 'OK', type: UserResponse })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  me(@GetUser() user: User): UserResponse {
     return this.authService.me(user);
   }
 }
